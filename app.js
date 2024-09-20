f1c596cb25738be4fa9a8c4072a65174c9e39b9e
shelja sherin m.v <114505403+sheljasherin@users.noreply.github.com>
2024-09-20 19:02:10 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import './myapp.css'
+import Counter from './Counter';
+  function App() {
+    return (
+    
+      <div style={{display:"flex",justifyContent:'center',flexDirection:"column",alignItems:"center"}}>
+      <h1>counter App</h1>
+      <Counter/>
+      </div>
+    
+  );
+}
+
+export default App;
